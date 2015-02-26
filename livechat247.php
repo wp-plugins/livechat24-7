b7aa6367c8915a22a28ae778e792ab55bd8db35b
botego <botego@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-26 10:04:27 +0000

    Adding first version of my plugin
    git-svn-id: https://plugins.svn.wordpress.org/livechat24-7/trunk@1100143 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ livechat247.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * Plugin Name:       livechat24-7
+ * Plugin URI:        https://www.livechat24-7.com/?ref=wp_plugin
+ * Description:       <a href="https://www.livechat24-7.com/">Live chat</a> that learns from you as you engage with your customers. Automatically builds your knowledge base and acts as a robot agent 24/7.
+ * Version:           1.0
+ * Author:            botego
+ * Author URI:        https://www.livechat24-7.com/?ref=wp_plugin
+ * Text Domain:       livechat247
+ */
+
+
+$plugurldir = get_option('siteurl') . '/' . PLUGINDIR . '/livechat247/';
+add_action('init', 'livechat247_init');
+add_action('wp_footer', 'livechat247_embed_code');
+add_action('admin_notices', 'livechat247_admin_notice');
+add_filter('plugin_action_links', 'livechat247_plugin_actions', 10, 2);
+
+function livechat247_init()
+    {
+    if (function_exists('current_user_can') && current_user_can('manage_options')) add_action('admin_menu', 'livechat247_add_settings_page');
+    if (!function_exists('get_plugins')) require_once (ABSPATH . 'wp-admin/includes/plugin.php');
+
+    $options = get_option('livechat247Disable');
+    }
+
+function livechat247_settings()
+    {
+    register_setting('livechat247-group', 'Livechat247ID');
+    register_setting('livechat247-group', 'livechat247Disable');
+    add_settings_section('livechat247', "Livechat24-7", "", 'livechat247-group');
+    }
+
+function plugin_get_version()
+    {
+    if (!function_exists('get_plugins')) require_once (ABSPATH . 'wp-admin/includes/plugin.php');
+
+    $plugin_folder = get_plugins('/' . plugin_basename(dirname(__FILE__)));
+    $plugin_file = basename((__FILE__));
+    return $plugin_folder[$plugin_file]['Version'];
+    }
+
+function livechat247_embed_code()
+    {
+    global $current_user;
+    if (get_option('Livechat247ID'))
+        {
+        echo ('
+    <!-- livechat24-7.com widget --> 
+    <script type="text/javascript"> 
+    window.$_liveChat247||function(a,b){b.$_liveChat247=function(a,b){var c=a.getElementsByTagName("head")[0];b._hostURL="https://www.livechat24-7.com";var d=a.createElement("script");d.src=b._hostURL+"/dashboard/liveChatIframe.js",d.type="text/javascript",c.appendChild(d),b._liveChat={},b._liveChat.profile={accountId:"' . get_option("Livechat247ID") . '"}},b.$_liveChat247(a,b)}(document,window);
+    </script> 
+    <!-- /livechat24-7.com widget -->');
+        }
+    }
+
+function livechat247_admin_notice()
+    {
+    if (!get_option('Livechat247ID')) echo ('<div class="error"><p><strong>' . sprintf(__('Livechat24-7 is disabled. Please go to the <a href="%s">plugin page</a> and enter a valid account ID to enable it.') , admin_url('options-general.php?page=livechat247')) . '</strong></p></div>');
+    }
+
+function livechat247_plugin_actions($links, $file)
+    {
+    global $plugurldir;
+    static $this_plugin;
+    if (!$this_plugin) $this_plugin = plugin_basename(__FILE__);
+    if ($file == $this_plugin && function_exists('admin_url'))
+        {
+        $settings_link = '<a href="' . admin_url('options-general.php?page=livechat247') . '">' . __('Settings') . '</a>';
+        array_unshift($links, $settings_link);
+        }
+
+    return ($links);
+    }
+
+function livechat247_add_settings_page()
+    {
+    global $plugurldir;
+    function livechat247_settings_page()
+        {
+        global $plugurldir;
+?>
+    <div class="wrap">
+        <?php screen_icon(); ?>
+        <h2>
+            <?php _e('Livechat24-7'); ?>
+            <small><?php echo plugin_get_version(); ?></small>
+        </h2>
+        <div class="metabox-holder meta-box-sortables ui-sortable pointer">
+            <div class="postbox" style="float:left;width:30em;left: -196px;margin-left: 50%;">
+                <h3 class="hndle"><span><?php _e('livechat24-7 Account ID'); ?></span></h3>
+                <div class="inside" style="padding: 0 10px">
+                    <p style="text-align:center">
+                        <a href="https://www.livechat24-7.com/">
+                            <img src="<?php echo ($plugurldir); ?>lc.png" height="39" width="159" alt="<?php _e('livechat24-7 Logo');?>" />
+                        </a>
+                    </p>
+                    <form method="post" action="options.php">
+                        <?php settings_fields('livechat247-group'); ?>
+                        <p>
+                            <label for="Livechat247ID">
+                                <?php printf(__('Enter your %1$slivechat24-7%2$s account ID below to activate the plugin.') , '<strong><a href="https://www.livechat24-7.com/dashboard/login.html?ref=wp_plugin" >', '</a></strong>'); ?>
+                            </label><br />
+                            <input type="text" name="Livechat247ID" value="<?php echo get_option('Livechat247ID'); ?>" style="width:100%" />
+                        </p>
+                        <p class="submit">
+                            <input type="submit" class="button-primary" value="<?php _e('Save Changes'); ?>" />
+                        </p>
+                    </form>
+                    
+                    <small class="nonessential"><?php _e('Entering an incorrect ID will result in an error!');?></small>
+                    <p style="font-size:smaller;color:#999239;background-color:#ffffe0;padding:0.4em 0.6em !important;border:1px solid #e6db55;-moz-border-radius:3px;-khtml-border-radius:3px;-webkit-border-radius:3px;border-radius:3px"><?php printf(__('%1$sRegister for a free livechat24-7 account today!%2$sRegister for a <strong>FREE</strong> livechat24-7 account right now!%3$s') , '<a href="https://www.livechat24-7.com/dashboard/login.html?ref=wp_plugin" title="', '">', '</a>'); ?></p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php 
+        }
+
+    add_action('admin_init', 'livechat247_settings');
+    add_submenu_page('options-general.php', __('Livechat24-7') , __('Livechat24-7') , 'manage_options', 'livechat247', 'livechat247_settings_page');
+    }
+
+?>
---
+++ readme.txt
@@ -0,0 +1,71 @@
+=== livechat24-7 ===
+Contributors: botego
+Donate link:
+Tags: automated live chat, free live chat, live agent, live chat, live chat software, mobile live chat, Customer Service, customer service software, Online Customer Service
+Requires at least: 3.0
+Tested up to: 3.5
+Stable tag: 1.0
+
+Live chat that learns from you as you engage with your customers. Automatically builds your knowledge base and acts as a robot agent 24/7.
+
+== Description ==
+
+**Customer support shouldn't go to sleep!**
+
+[Live chat 24/7](https://www.livechat24-7.com) is customer service re-imagined. It learns from your conversations as you engage with your customers. Automatically builds your knowledgebase and acts as a self-serving super-agent that delivers most relevant answers 24/7.
+
+[youtube https://www.youtube.com/watch?v=8shHwnHUcqg]
+
+**Automated training**
+
+Question & Answer sets are automatically generated as you answer customer questions. Our award-winning natural language processing engine connect questions with relevant keywords. You can remove unwanted content with one click.
+
+**Seamlessly switch between automated and live agents**
+
+Conveniently switch to automated agents after business hours and statutory holidays. Basically, any time you’ll be away, your robot agents are there 24/7 to help your customers out. You can go back to live agents with one simple click.
+
+**We’ll send you potential customers, looking for your products and services!**
+
+We automatically generate a Frequently Asked Questions page with questions & answers from your live chat application. As search engines crawl those pages, you’ll easily attract visitors to your website who are specifically looking to buy your products and services.
+
+**And all other cool features a live chat software should have**
+
+Any basic and advanced features you would expect from a top quality live chat software: Multiple agent accounts and departments, queue system, detailed analytic reports, chat logs, and more.
+
+**About Botego**
+
+Botego is the leading developer of language technologies with 7+ year experience within the field. Clients include Coca Cola, Johnson&Johnson, FOX and PepsiCO. We help companies save thousands of hours every month, and increase customer satisfaction. Our patent-pending natural language processing engine empowers ordinary applications.
+
+== Installation ==
+
+**Step-by-step Guide:**
+
+1. Go to the plugings page and activate livechat24-7.
+2. Click the settings link of livechat24-7
+3. Enter your livechat24-7 account ID and then go to the frontend of your website to see your new chat widget in action!
+
+
+== Frequently Asked Questions ==
+
+= Is it free? =
+
+Yes, it's completeley free with one live agent. If you need more, it's only $29/mo (with longer chat history log and e-mail support)
+
+= How does it work? =
+
+It simply learns from the conversations as you engage with your customer. And builds a knowledge base  to start delivering the most relevant answers automatically.
+
+= How can I sign up? =
+
+Simply follow the instructions and sign up with one clich using your social media accounts! 
+== Screenshots ==
+
+1. /assets/screenshot-1.png
+2. /assets/screenshot-2.png
+3. /assets/screenshot-3.png
+4. /assets/screenshot-4.png
+
+== Changelog ==
+
+= 1.0 =
+* The first version
